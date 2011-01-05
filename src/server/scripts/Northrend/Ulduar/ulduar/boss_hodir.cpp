0b2b2f829c94a8ba4578b61dd90df076c80fcbf4
Retriman <nicolas_katre@hotmail.com>
2011-01-05 13:49:48 -0300

    Fixed typo anterior commit.

--- src/server/scripts/Northrend/Ulduar/ulduar/boss_hodir.cpp
+++ src/server/scripts/Northrend/Ulduar/ulduar/boss_hodir.cpp
@@ -527,7 +527,7 @@ public:
             if (DispelTimer < uiDiff)
             {
                 std::list<Player*> players;
-                Trinity::AnyPlayerInObjectRangeCheck checker(me, 30);
+                Trinity::AnyPlayerInObjectRangeCheck checker(me, 30, true);
                 Trinity::PlayerListSearcher<Trinity::AnyPlayerInObjectRangeCheck> searcher(me, players, checker);
                 me->VisitNearbyWorldObject(30, searcher);
                 if (!players.empty())
@@ -599,7 +599,7 @@ public:
             if (StormTimer <= uiDiff)
             {
                 std::list<Player*> players;
-                Trinity::AnyPlayerInObjectRangeCheck checker(me, 30);
+                Trinity::AnyPlayerInObjectRangeCheck checker(me, 30, true);
                 Trinity::PlayerListSearcher<Trinity::AnyPlayerInObjectRangeCheck> searcher(me, players, checker);
                 me->VisitNearbyWorldObject(30, searcher);
                 if (!players.empty())
