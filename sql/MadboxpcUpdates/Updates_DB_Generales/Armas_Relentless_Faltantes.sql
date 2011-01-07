a86842cd525b91699e876903fc46e415c9e59d96
Retriman <nicolas_katre@hotmail.com>
2011-01-07 18:30:37 -0300

    WOWMadboxPC/Database - Añadido Armas Faltantes (Armas Relentless 3v3). Issue 16. https://github.com/Retriman/MadboxpcWOW-core-/issues#issue/16

---
+++ sql/MadboxpcUpdates/Updates_DB_Generales/Armas_Relentless_Faltantes.sql
@@ -0,0 +1,19 @@
+-- Solucionando Problema de Armas Faltantes 3vs3.
+SET @NPC = 34090;
+DELETE FROM `npc_vendor` WHERE `entry`=@NPC AND `item` IN (48511,48438,48440,48509,48519,48435,48513,48521,48444,48507,48517,48523,48515,48442,49191);
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`) VALUES
+(@NPC, 0, 48511, 0, 0, 1758),-- http://www.wowhead.com/item=48511 - Relentless Gladiator's Truncheon
+(@NPC, 0, 48438, 0, 0, 1758),-- http://www.wowhead.com/item=48438 - Relentless Gladiator's Swiftblade
+(@NPC, 0, 48440, 0, 0, 1758),-- http://www.wowhead.com/item=48440 - Relentless Gladiator's Splitter
+(@NPC, 0, 48509, 0, 0, 1758),-- http://www.wowhead.com/item=48509 - Relentless Gladiator's Spike
+(@NPC, 0, 48519, 0, 0, 1758),-- http://www.wowhead.com/item=48519 - Relentless Gladiator's Salvation 
+(@NPC, 0, 48435, 0, 0, 1758),-- http://www.wowhead.com/item=48435 - Relentless Gladiator's Punisher
+(@NPC, 0, 48513, 0, 0, 1758),-- http://www.wowhead.com/item=48513 - Relentless Gladiator's Longblade
+(@NPC, 0, 48521, 0, 0, 1758),-- http://www.wowhead.com/item=48521 - Relentless Gladiator's Light Staff 
+(@NPC, 0, 48444, 0, 0, 1758),-- http://www.wowhead.com/item=48444 - Relentless Gladiator's Left Claw
+(@NPC, 0, 48507, 0, 0, 1758),-- http://www.wowhead.com/item=48507 - Relentless Gladiator's Handaxe
+(@NPC, 0, 48517, 0, 0, 1758),-- http://www.wowhead.com/item=48517 - Relentless Gladiator's Halberd
+(@NPC, 0, 48523, 0, 0, 1758),-- http://www.wowhead.com/item=48523 - Relentless Gladiator's Greatstaf
+(@NPC, 0, 48515, 0, 0, 1758), -- http://www.wowhead.com/item=48515 - Relentless Gladiator's Grasp
+(@NPC, 0, 48442, 0, 0, 1758), -- http://www.wowhead.com/item=48442 - Relentless Gladiator's Eviscerator
+(@NPC, 0, 49191, 0, 0, 1758);-- http://www.wowhead.com/item=49191 - Relentless Gladiator's Blade of Celerity
--- sql/MadboxpcUpdates/Updates_DB_Generales/Npc_Relentless.sql
+++
@@ -1,7 +0,0 @@
--- Añadido Vendedores Faltantes  armas Relentless 3v3 - 5v5 (lvl 258) reportado en http://code.google.com/p/madboxpcwow/issues/detail?id=158
-DELETE FROM `creature` WHERE `guid` IN (718654,718672);
-INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `DeathState`, `MovementType`) VALUES
-(718654, 34090, 571, 1, 1, 0, 2100, 5751.81, 586.383, 614.969, 0.930669, 300, 0, 0, 6986, 0, 0, 0),
-(718672, 34091, 571, 1, 1, 0, 0, 5751.55, 584.302, 614.969, 5.49776, 300, 0, 0, 6986, 0, 0, 0);
--- Clean
-DELETE FROM `creature_addon` WHERE `guid` NOT IN (SELECT `guid` FROM `creature`);
