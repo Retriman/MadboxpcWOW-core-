8e3e2c08760f9444c69b97e42ff0e6c5c19b3b6a
Barneysk8 <adrian_mardones@hotmail.com>
2011-01-10 20:03:36 -0300

    [WOWMadboxPC/DB] *Quest Zuluhed the Whacked

---
+++ sql/MadboxpcUpdates/Quest/Shadowmoon Valley/Quest_Zuluhed_the_whacked.sql
@@ -0,0 +1,2 @@
+-- Fix quest 10866 - Zuluhed the Whacked 
+UPDATE `quest_template` SET `ReqCreatureOrGOId1` = -185156, `ObjectiveText1` = 'Karynaku freed', `ReqCreatureOrGOId2` = 11980 , `ObjectiveText2` = 'Zuluhed the Whacked slain'  WHERE entry = 10866;
