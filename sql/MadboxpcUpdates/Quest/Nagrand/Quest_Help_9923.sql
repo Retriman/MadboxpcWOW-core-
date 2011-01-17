0726efadbb800f0db94e37a600426bbb8a1b7916
Retriman <nicolas_katre@hotmail.com>
2011-01-17 20:08:41 -0300

    [WOWMadboxPC/DB] Añadido Full Faltantes.

--- sql/MadboxpcUpdates/Quest/Nagrand/Quest_Help_9923.sql
+++ sql/MadboxpcUpdates/Quest/Nagrand/Quest_Help_9923.sql
@@ -1,4 +1,4 @@
---  Quest Soporte http://www.wowhead.com/quest=9923 "HELP!"
+-- Quest Soporte http://www.wowhead.com/quest=9923 "HELP!"
 DELETE FROM `script_texts` WHERE `npc_entry`=18445;
 INSERT INTO `script_texts` (`npc_entry`, `entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`) VALUES
 ('18445','-1850070','Thanks,$R Im sure my dad will reward you greatly,bye',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'0','0','0','0','');
