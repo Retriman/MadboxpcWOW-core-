68696f3b5cc858d4ac10a5c1a2f8a21ce969d908
Retriman <nicolas_katre@hotmail.com>
2011-01-25 15:13:05 -0300

    [WOWMadboxPC/DB] Actualizado Full Update

--- sql/MadboxpcUpdates/Updates/Full_Update.sql
+++ sql/MadboxpcUpdates/Updates/Full_Update.sql
@@ -240,3 +240,17 @@ INSERT INTO `spell_pet_auras` (`spell`, `effectId`, `pet`, `aura`) VALUES
 ('34459','0','0','75446'), -- Ferocious Inspiration (Rank 2)
 ('34460','0','0','75447'); -- Ferocious Inspiration (Rank 3)
 
+-- 24_01_2010_world_spell_bonus_data.sql
+-- Fix Talent - Naturalist - Druida spell=17069
+DELETE FROM `spell_bonus_data` WHERE `entry` IN (779,780,769,9754,9908,26997,48561,48562);
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`,`ap_bonus`, `ap_dot_bonus`, `comments`) VALUES 
+(779,0,0,0.063,0,'Druid - Swipe (Bear) Rank 1'),
+(780,0,0,0.063,0,'Druid - Swipe (Bear) Rank 2'),
+(769,0,0,0.063,0,'Druid - Swipe (Bear) Rank 3'),
+(9754,0,0,0.063,0,'Druid - Swipe (Bear) Rank 4'),
+(9908,0,0,0.063,0,'Druid - Swipe (Bear) Rank 5'),
+(26997,0,0,0.063,0,'Druid - Swipe (Bear) Rank 6'),
+(48561,0,0,0.063,0,'Druid - Swipe (Bear) Rank 7'),
+(48562,0,0,0.063,0,'Druid - Swipe (Bear) Rank 8');
+
+
