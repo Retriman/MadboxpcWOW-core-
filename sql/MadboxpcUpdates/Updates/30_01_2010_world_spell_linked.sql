7ecd85ee2b9adb7870c4cb6b6da5077012be478b
Retriman <nicolas_katre@hotmail.com>
2011-01-30 22:55:10 -0300

    fixed typo anterior commit sorry.

--- sql/MadboxpcUpdates/Updates/30_01_2010_world_spell_linked.sql
+++ sql/MadboxpcUpdates/Updates/30_01_2010_world_spell_linked.sql
@@ -1,4 +1,4 @@
 --  http://www.wowhead.com/spell=11129 Combustion Stack Limit.
-DELETE FROM `spell_linked_spell` WHERE `spell_trigger`='28682' AND `spell_effect` ='-28682';
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger`='-11129' AND `spell_effect` ='-28682';
 INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
 (-11129, -28682, 0, 'Combustion remove stack limit');
